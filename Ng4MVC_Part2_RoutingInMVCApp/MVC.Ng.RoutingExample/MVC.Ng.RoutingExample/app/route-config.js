4905951454ab2c386cccaa6ac6a3348c2b8f0a4a
btroncone <btroncone@gmail.com>
2014-08-24 16:10:13 -0400

    Updated for more clarity

--- Ng4MVC_Part2_RoutingInMVCApp/MVC.Ng.RoutingExample/MVC.Ng.RoutingExample/app/route-config.js
+++ Ng4MVC_Part2_RoutingInMVCApp/MVC.Ng.RoutingExample/MVC.Ng.RoutingExample/app/route-config.js
@@ -5,6 +5,9 @@
 
     function config($routeProvider) {
         $routeProvider
+            .when('/', {
+                templateUrl: '/app/views/default.html'
+            })
             .when('/viewOne', {
                 templateUrl: '/app/views/viewOne.html',
                 //controller: 'yourController',
